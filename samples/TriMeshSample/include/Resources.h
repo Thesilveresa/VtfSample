ecddde8bae3c093d0d3b0bf4bcdb6dbeab374d08
Thesilveresa <theresa.silver@gmail.com>
2013-08-14 22:46:54 -0400

    first commit

---
+++ samples/TriMeshSample/include/Resources.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "cinder/CinderResources.h"
+
+#define RES_TEXTURE		CINDER_RESOURCE( ../resources/, texture.png, 128, PNG )
+#define RES_TEX2		CINDER_RESOURCE( ../resources/, 1.png, 129, PNG )
---
+++ samples/VtfSample/include/Resources.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "cinder/CinderResources.h"
+
+#define RES_SHADER_TEX_FRAG		CINDER_RESOURCE( ../resources/, tex_frag.glsl,	128, GLSL	)
+#define RES_SHADER_TEX_VERT		CINDER_RESOURCE( ../resources/, tex_vert.glsl,	129, GLSL	)
+#define RES_SHADER_VTF_FRAG		CINDER_RESOURCE( ../resources/, vtf_frag.glsl,	130, GLSL	)
+#define RES_SHADER_VTF_VERT		CINDER_RESOURCE( ../resources/, vtf_vert.glsl,	131, GLSL	)
+#define RES_TEXTURE				CINDER_RESOURCE( ../resources/, texture.png,	132, PNG	)
+#define RES_TEXTURE2			CINDER_RESOURCE( ../resources/, 1.jpg,        133, jpg	)
+#define RES_TEXTURE3			CINDER_RESOURCE( ../resources/, 4.png,          134, PNG	)
